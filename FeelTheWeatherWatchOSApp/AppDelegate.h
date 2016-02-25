2d8eadf4f6dd74953df5aba425307e520e748cd5
ayunav <ayunanyc@gmail.com>
2016-02-24 21:29:37 -0500

    Initial Commit

---
+++ FeelTheWeatherWatchOSApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeelTheWeatherWatchOSApp
+//
+//  Created by Ayuna Vogel on 2/24/16.
+//  Copyright © 2016 Ayuna Vogel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
